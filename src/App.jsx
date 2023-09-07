151881d3654f65122e99b122b0239bb3a5ba1df9
Pedro Almeida <pedrog-almeida@hotmail.com>
2023-09-06 23:10:19 -0300

    adding products

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header'
+import Products from './components/Products/Products'
 
 function App() {
   return (
     <div>
       <Header />
+      <Products />
     </div>
   )
 }
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,34 @@
+import { BsFillCartPlusFill } from 'react-icons/bs'
+import propTypes from 'prop-types'
+import './ProductCard.css'
+import formatCurrency from '../../utils/formatCurrency';
+
+function ProductCard({ data }) {
+
+  const { title, thumbnail, price } = data;
+
+  return (
+    <section className="product-card">
+      <img
+        src={thumbnail.replace(/\w\.jpg/gi, "W.jpg")}
+        alt="products"
+        className="card__image"
+      />
+
+      <div className="card__infos">
+        <h2 className="card__price">{formatCurrency(price, 'BRL')}</h2>
+        <h2 className="card__title">{title}</h2>
+      </div>
+
+      <button type="button" className="button__add-cart">
+        <BsFillCartPlusFill />
+      </button>
+    </section>
+  );
+}
+
+export default ProductCard;
+
+ProductCard.propTypes = {
+  data: propTypes.shape({})
+}.isRequired;
